a4b8a6726cabc948b3f21b917e70ed028ba22aa9
Lyzz <amelie_du89@hotmail.fr>
2020-09-12 18:41:45 +0200

    2nd commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="abc">
+        <nav class="navbar navbar-expand-lg navbar-dark ">
+            <a class="navbar-brand" href="#">Accueil</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="formulaire.php">Formulaire <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="card.php">Carte</a>
+                    </li>
+            </div>
+        </nav>
+    </div>
+    <div class="carousselmaison">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="https://miro.medium.com/max/700/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="https://image.freepik.com/photos-gratuite/image-du-cerveau-humain_99433-298.jpg" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+    <div class="textlorem">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quibusdam voluptate accusantium optio accusamus ex doloribus beatae, laboriosam culpa eos consequatur debitis repudiandae dignissimos provident quas voluptas, magnam similique voluptatum.</p>
+        <p>Loremphkjhhh hj ipsum dolor sit amet, consectetur adipisicing elit. At nemo corporis veniam consequuntur nesciunt minus animi, vel tempore architecto vitae temporibus aliquid, esse quidem culpa dolor. Commodi tempora quos sunt.</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus temporibus soluta aut praesentium odit iste ab provident eveniet et similique laudantium magnam ipsa sint, at sed, neque velit, vero fugit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores accusamus exercitationem quasi similique quaerat vel magni nostrum provident tempore debitis, suscipit voluptatum ipsam officiis, dolorum esse non sint doloremque totam.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae minus natus autem minima soluta necessitatibus molestiae iure iusto rerum ratione ut, nam fugit et voluptatibus doloribus laboriosam ducimus excepturi laudantium?</p>
+    </div>
+</body>
+<script src="/vendor\components\jquery/jquery.min.js"></script>
+<script src="/vendor\twbs\bootstrap\dist\js/bootstrap.min.js"></script>
+
+</html>
